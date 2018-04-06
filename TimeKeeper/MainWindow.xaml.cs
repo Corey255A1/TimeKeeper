4fc4e4c4305a23f1d4792a2a5692e564201c828c
Corey <Corey255A1@hotmail.com>
2018-04-06 13:26:02 -0400

    Initial Commit

---
+++ TimeKeeper/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TimeKeeper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TimeTicker theTicker;
+        public MainWindow()
+        {
+            InitializeComponent();
+            theTicker = new TimeTicker();
+            theTicker.TickEvent += Tick;
+
+        }
+        public void Tick(DateTime t)
+        {
+            Dispatcher.Invoke(new Action(() =>
+            {
+                testTimeLbl.Text = t.ToLongTimeString();
+            }));
+            
+        }
+    }
+}
---
+++ TimeKeeper/TimeTicker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace TimeKeeper
+{
+    public delegate void TickCallback(DateTime time);
+    class TimeTicker
+    {
+        public event TickCallback TickEvent;
+
+        Timer theTicker;
+        public TimeTicker()
+        {
+            theTicker = new Timer(Tick, this, 1000, 1000); 
+        }
+        private void Tick(object obj)
+        {
+            //If(TickEvent != null) TickEvent(DateTime.UtcNow);
+            TickEvent?.Invoke(DateTime.UtcNow);
+        }
+    }
+}
