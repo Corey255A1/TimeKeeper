a1cf734b51f63d9811e46c51d6eccc38d4879a87
Corey <Corey255A1@hotmail.com>
2018-04-16 07:59:27 -0400

    Missed this file on check in..

---
+++ TimeKeeper/ChargeCode.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+namespace TimeKeeper
+{
+
+    public class ChargeCode
+    {
+        [XmlAttribute]
+        public string Code;
+
+        [XmlText]
+        public string Description;
+    }
+
+    [Serializable]
+    [XmlRoot("ChargeCodes")]
+    public class ChargeCodeFile
+    {
+        [XmlElement("ChargeCode")]
+        public List<ChargeCode> ChargeCode;
+
+        public ChargeCodeFile()
+        {
+            ChargeCode = new List<ChargeCode>();
+        }
+
+        public static ChargeCodeFile ReadFile(string filename)
+        {
+            XmlSerializer xs = new XmlSerializer(typeof(ChargeCodeFile));
+            using (var reader = new StreamReader(filename))
+            {
+                var ccf = (ChargeCodeFile)xs.Deserialize(reader);
+
+                return ccf;
+            }
+
+        }
+        public void WriteFile(string filename)
+        {
+            XmlSerializer xs = new XmlSerializer(typeof(ChargeCodeFile));
+            using (var writer = new StreamWriter(filename))
+            {
+                xs.Serialize(writer, this);
+            }
+
+        }
+    }
+}
